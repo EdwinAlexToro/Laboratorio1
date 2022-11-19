2693cf3a57436e4b8986b8f60fa20a0455b411b6
EdwinAlexToro <edwin.alextoro@gmail.com>
2022-11-19 16:55:34 -0500

    mejoras al formulario y un poco de codigo al script de ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,41 @@
-// $(function(){
-//     $("#flex__container--aside").fadeOut();
-//     $("#bars").click(function(){
-//         $("#flex__container--aside").fadeIn();
-//     });
-// });
+var inpAjax = document.querySelector('#inpAjax');
+var btnAjax = document.querySelectorAll('#btnAjax');
+var reqStatus = document.querySelector('#reqStatus');
+var resAjax = document.querySelector('#resAjax');
+
+
+//se crea el objeto para realizar la peticion
+
+var xhr = new XMLHttpRequest();
+
+//Se obtiene el elemento del documento donde ira la respuesta en la pagina
+
+var resAjax = document.getElementById('resAjax');
+
+//Se crea el fracmento que hará parte del documento
+
+var fracmento = document.createDocumentFragment();
+
+btnAjax.addEventListener('oneclick',function(){
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.onload = (e) => {
+    if(xhr.readyState === 4){
+        if(xhr.status == 200){
+            console.log(xhr.responseText);
+        }
+
+    }else{
+        console.error(xhr.statusText);
+
+    }
+
+};
+xhr.onerror = (e) =>{
+    console.error(xhr.statusText);
+};
+xhr.send();
+
+});
+
+
+
