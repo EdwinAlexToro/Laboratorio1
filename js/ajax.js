23424809646995d9125313350a28efc830f74f4f
EdwinAlexToro <edwin.alextoro@gmail.com>
2022-11-21 22:26:57 -0500

    formulario ajax completado

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,11 @@
+
 var inpAjax = document.querySelector('#inpAjax');
 var btnAjax = document.querySelectorAll('#btnAjax');
+var btnClear = document.querySelectorAll('#btnClear');
 var state__peticion = document.querySelector('#state__peticion');
+var http__code__container = document.querySelector('.http__code--container');
 var http__response = document.querySelector('#http__response');
+var ajax__response__container = document.querySelector('.ajax__response--container');
 var ajax__response = document.querySelector('#ajax__response');
 
 
@@ -9,9 +13,11 @@ var ajax__response = document.querySelector('#ajax__response');
 
 function peticionAjax(url){
 
-    ///Condicional para comprobar la URL
+    ///Condicional para comprobar la inpAjax
     
-    if(url == "" || url == 0 || url == null ){
+    
+    if(inpAjax == "" || inpAjax == 0 || inpAjax == null ){
+        document.write("Ingreso al error");
 
         state__peticion.innerHTML = "Comporbando";
 
@@ -19,15 +25,18 @@ function peticionAjax(url){
 
             console.log("Error");
             state__peticion.innerHTML = "Error";
-            $(".http__code--container").fadeIn();
-            http__response.innerHTML = "Ingresa una URL valida";
-
+            $(http__code__container).fadeIn();
+            http__response.innerHTML = "Ingresa una inpAjax valida";
+            $(btnAjax).fadeOut();
+            $(btnClear).fadeIn();
+            
         },500);
 
         
         //Comprobar si hay algun resultado en pantalla
         //Antes de iniciar una nueva peticion
-    }else if(state__peticion == ""){
+    }else if(state__peticion.innerHTML == "" ||state__peticion.innerHTML == "Sin iniciar"){
+        console.log("Ingreso a la creacion");
         (() => { 
 
             //Crear objeto XHR
@@ -37,7 +46,7 @@ function peticionAjax(url){
             const $xhr = document.getElementById("ajax__response");
 
             //Crea un fracmento de documento el cual sera insertado en el documento con la respuesta del servido
-            $fragmento = document.createDocumentFragment();
+            const $fragmento = document.createDocumentFragment();
 
             //Muestra por consola el objeto XHR
             console.log(XHR);
@@ -54,16 +63,17 @@ function peticionAjax(url){
 
                         console.log("Cargando...");
                         return state__peticion.innerHTML = "Cargando...";
-                        },500);
+                        },250);
                 }
                 //Comprobar estado completado
-                if(XHR.readyState == 4){
+                if(XHR.readyState !== 4){
 
                     setTimeout(function(){
 
                         console.log("Completado...");
-                        return state__peticion.innerHTML = "Completado...";
+                        state__peticion.innerHTML = "Completado...";
                     },500)
+                    return;
                 }
                 //Comprobar y mostrar el resultado de cabeceras de peticion
                 setTimeout(function(){
@@ -76,13 +86,14 @@ function peticionAjax(url){
                         let ajaxRes = JSON.parse(XHR.responseText);
                         console.log(ajaxRes);
 
-                        ajaxRes.forEach(element => {
+                        ajaxRes.forEach((element) => {
+
                             const $li = document.createElement("li");
                             $li.className = "lista";
-                            $li.innerHTML = `
+                            $li.innerHTML = `<br>
                             Nombre: ${element.name} <br>
                             Email: ${element.email} <br>
-                            Télefono: ${element.phone} <br>`;
+                            Télefono: ${element.phone} <br><br>`;
 
                             $fragmento.appendChild($li);
                             
@@ -90,24 +101,52 @@ function peticionAjax(url){
 
 
                         $xhr.appendChild($fragmento);
+                        $(ajax__response__container).fadeIn();
 
 
                     //De lo contrario Muestra el error y la respuesta del servidor
                     }else{
-
+                        console.log("Error");
+                        state__peticion.innerHTML = "Error";
+                        http__response.innerHTML = `${XHR.status}: Error en la Url..."`;
+                        $(http__code__container).fadeIn();
+                        
                     }
                     //Cambio de boton mostrar a boton borrar
+                
+                    $(btnAjax).fadeOut();
+                    $(btnClear).fadeIn();
 
-                },700);
+                },750);
 
             });
             //Metodos send y open
-        
+            XHR.open("GET", url);
+            XHR.send();
+            
         })();
+    }else{
+        document.write("No Ingreso");
     }
-
-
 };
+document.addEventListener("DOMContentLoaded", function(event) {
+$(btnClear).fadeOut();
+$(http__code__container).fadeOut();
+$(ajax__response__container).fadeOut();
+
+// btnAjax.addEventListener('click',function () {
+btnAjax[0].addEventListener("click", function (){
+    peticionAjax(inpAjax.value);
+});
+
+btnClear[0].addEventListener("click",function () {
+    ajax__response.innerHTML="";
+    http__response.innerHTML="";
+    state__peticion.innerHTML= "Sin iniciar";
+    $(btnAjax).fadeIn();
+    $(btnClear).fadeOut();
+});
+});
 
 
 
